ca1098b08f35287683b00244e81eb9ec0d513c0c
tcrurav <tiburcio.cruz@gmail.com>
2021-12-03 10:48:32 +0000

    dotenv added to backend

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,13 +1,15 @@
+require('dotenv').config();
+
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "sasa",
-  DB: "db_bicycles",
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
+    HOST: process.env.DB_HOST,
+    USER: process.env.MYSQL_USER,
+    PASSWORD: process.env.MYSQL_PASSWORD,
+    DB: process.env.MYSQL_DATABASE,
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
 };
